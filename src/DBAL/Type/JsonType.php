b525aa4e8e8162bbfd22a20f0d39f6d3e4de3fad
Erwan Richard <erwan.richard@lephare.com>
2017-12-26 11:03:47 +0100

    Add support for JSONB type

---
+++ src/DBAL/Type/JsonType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Doctrine\DBAL;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\JsonType as BaseJsonType;
+
+class JsonType extends BaseJsonType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    {
+        return 'JSONB';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'jsonb';
+    }
+}
